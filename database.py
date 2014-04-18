210a352cf70d9aa046bfebe6091ca85e4c70c05e
Ben Oswald <ben.oswald@root-space.de>
2014-04-18 17:20:31 +0200

    add database model

---
+++ database.py
@@ -0,0 +1,17 @@
+from sqlalchemy import create_engine, Column, Integer, String
+from sqlalchemy.ext.declarative import declarative_base
+
+engine = create_engine('sqlite://nodes.db')
+Base = declarative_base()
+
+class Nodes(Base):
+    __tablesname__ = 'nodes'
+
+    nodeID = Column(Integer, primary_key=True)
+    name = Column(String, unique=True)
+    fastdkey = Column(String, unique=True)
+    mac = Column(String, unique=True)
+    nick = Column(String)
+    email = Column(String)
+    coord = Column(String)
+
