dba1ed2c8f34b489dc59c276838a38ee7de8d348
Ben Oswald <ben.oswald@root-space.de>
2014-05-01 17:22:04 +0200

    add code to handle valid input

--- backend.py
+++ backend.py
@@ -1,9 +1,12 @@
 from flask import Flask, request, jsonify, render_template, abort
 import json
-from helper import InputParser
+from helper import InputParser, Token
+import db as database
 
 app = Flask(__name__)
 parser = InputParser()
+tokgen = Token()
+db = database.DB()
 
 @app.route('/')
 def main_site():
@@ -17,6 +20,12 @@ def process_data():
             resp = jsonify(**vres)
             resp.status_code = 400
             return resp
+        else:
+            resp = parser.getData(request)
+            resp['token'] = tokgen.getToken()
+            db.addNode(resp)
+            resp['status'] = 'success'
+            return jsonify(**resp)
     else:
         abort(400)
 
