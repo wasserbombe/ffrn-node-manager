5a4ff0c095491d2f0b4e30a664eba882e97d039a
Ben Oswald <ben.oswald@root-space.de>
2014-05-01 14:15:47 +0200

    add validation code and remove demo code

--- backend.py
+++ backend.py
@@ -1,20 +1,25 @@
-from flask import Flask, request, jsonify, render_template
+from flask import Flask, request, jsonify, render_template, abort
 import json
+from helper import InputParser
+
 app = Flask(__name__)
+parser = InputParser()
 
 @app.route('/')
 def main_site():
         return render_template('index.html')
 
 @app.route('/api/node', methods=['POST'])
-def print_data():
+def process_data():
     if request.method == 'POST':
-        node = request.form.to_dict();
-        print(node)
-        node['status'] = 'success'
-        node['token'] = '1234567abcdef'
-        print(json.dumps(node))
-    return jsonify(node)
+        vres = parser.validate(request)
+        print(vres)
+        if vres:
+            resp = jsonify(**vres)
+            resp.status_code = 400
+            return resp
+    else:
+        abort(400)
 
 if __name__ ==  "__main__":
     app.run(debug=True)
