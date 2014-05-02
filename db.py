9d9fca9bf95df3e4879b4dd6d3ec25541bb20e7e
Ben Oswald <ben.oswald@root-space.de>
2014-05-02 13:27:41 +0200

    considered that it is useful to also provide the db.py
    adding the db.py I've forgotten in previous commits. Sorry!

---
+++ db.py
@@ -0,0 +1,66 @@
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker, mapper
+import sqlalchemy.exc
+import tables
+
+class DB(object):
+    def __init__(self):
+        Session = sessionmaker(bind=tables.engine)
+        self.session = Session()
+
+    def __row2dict(self, row):
+        """transform sqlalchemy row proxy into an dict"""
+        if not row:
+            raise ValueError("None isn't a valid value")
+        d = {}
+        for column in row.__table__.columns:
+            d[column.name] = getattr(row, column.name)
+        return d
+
+    def addNode(self, node):
+        try:
+            self.session.add(tables.Nodes(**node))
+            self.session.commit()
+        except:
+            self.session.rollback()
+            raise
+
+    def getNode(self, token):
+        try:
+            return self.__row2dict(self.session.query(tables.Nodes).filter(tables.Nodes.token == token).one())
+        except:
+            raise
+
+    def updateNode(self, node):
+        try:
+            self.session.query(tables.Nodes).filter(tables.Nodes.token == node['token']).update(node)
+            self.session.commit()
+        except:
+            raise
+
+    def __checkValue(self, column, value, token):
+        if token:
+            if self.session.query(tables.Nodes).filter(tables.Nodes.token != token).filter(column == value).first():
+                return True
+            else:
+                return False
+        else:
+            if self.session.query(tables.Nodes).filter(column == value).first():
+                return True
+            else:
+                return False
+
+    def checkHostname(self, hostname, token):
+        return self.__checkValue(tables.Nodes.hostname, hostname, token)
+
+    def checkMAC(self, mac, token):
+        return self.__checkValue(tables.Nodes.mac, mac, token)
+
+    def checkKey(self, key, token):
+        return self.__checkValue(tables.Nodes.key, key, token)
+
+    def checkToken(self, token):
+        if self.session.query(tables.Nodes).filter(tables.Nodes.token == token).first():
+            return True
+        else:
+            return False
