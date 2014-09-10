0eef354146a6017abe23236432bcac2e3aa2cf48
Ben Oswald <ben.oswald@root-space.de>
2014-09-10 22:47:25 +0200

    reformat the registration mail

--- mail.py
+++ mail.py
@@ -6,21 +6,28 @@ def getMail(node):
     msg.body = '''
 Hey {nickname},
 
-cool, du hast gerade einen neuen Knoten für das Netz von Freifunk Rhein Neckar registriert.
-Die Daten deines Knotens lauten:
-Hostname: {hostname}
-MAC: {mac}
-VPN Key: {key}
-Koordinaten: {coords}
-Nick: {nickname}
-Mail: {email}
-Token: {token}
+Du hast gerade einen neuen Knoten für das Netz von Freifunk Rhein Neckar registriert - cool!
 
-Wir hoffen das du gut zurecht gekommen bist und würden uns freuen dich auch mal persönlich kennen zu lernen.
-Solltest du Probleme oder Anregungen haben findest du die Kontaktmöglichkeiten im Wiki unter https://wiki.freifunk-rhein-neckar.de/doku.php/kontakt
+Die Daten deines Knotens lauten:
+
+    Hostname:       {hostname}
+    MAC:            {mac}
+    VPN Key:        {key}
+    Koordinaten:    {coords}
+    Nick:           {nickname}
+    Mail:           {email}
+    Token:          {token}
+
+Bitte halte diese Daten aktuell. Du kannst Sie unter [1] pflegen.
+
+Wir hoffen, dass Du gut zurecht gekommen bist und würden uns freuen, wenn Du dich auch persönlich in die Community einbringen würdest. Vielleicht kannst Du ja sogar noch weitere Leute von unserem Projekt überzeugen.
+
+Solltest du Probleme oder Anregungen haben, kannst Du entweder auf diese Email antworten oder uns mittels einer der Kontaktmöglichkeiten im Wiki [1] erreichen.
 
 Viele Grüße,
-
 Freifunk Rhein Neckar
+
+[1] https://register.freifunk-rhein-neckar.de/
+[2] https://w.ffrn.de/kontakt
 '''.format(**node)
     return msg
