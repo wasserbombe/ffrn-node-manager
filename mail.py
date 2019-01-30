265cd126daed7c40f3fadfc9909a4876110d729c
Sebastian <github@sebastianfuhrmann.de>
2019-01-30 20:58:07 +0100

    update link to contact options

--- mail.py
+++ mail.py
@@ -21,12 +21,12 @@ Bitte halte diese Daten aktuell. Du kannst Sie unter [1] pflegen.
 
 Wir hoffen, dass Du gut zurecht gekommen bist und würden uns freuen, wenn Du dich auch persönlich in die Community einbringen würdest. Vielleicht kannst Du ja sogar noch weitere Leute von unserem Projekt überzeugen.
 
-Solltest du Probleme oder Anregungen haben, kannst Du entweder auf diese Email antworten oder uns mittels einer der Kontaktmöglichkeiten im Wiki [2] erreichen.
+Solltest Du Probleme oder Anregungen haben, kannst Du entweder auf diese Email antworten oder uns über einen unserer Kommunikationswege (siehe FAQ [2]) erreichen.
 
 Viele Grüße,
 Freifunk Rhein Neckar
 
 [1] https://register.freifunk-rhein-neckar.de/
-[2] https://w.ffrn.de/kontakt
+[2] https://forum.ffrn.de/t/welche-kommunikationskanaele-habt-ihr/343
 '''.format(**node)
     return msg
